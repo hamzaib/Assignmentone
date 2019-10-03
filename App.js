da6875c3f5150ec2e89d965a3310b6aac52bf469
hamza Ibrahimi <hamza_ib2@hotmail.com>
2019-10-02 20:31:50 -0400

    bottom navigation

--- App.js
+++ App.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {createAppContainer} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
 
-export default class App extends Component {
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text>Open up App.js to start working on your app!</Text>
-            </View>
-        );
-    }
-}
+
+import HomeScreen from './home';
+import SettingScreen from './setting';
+import AboutScreen from './about';
+
+const tabNavigator = createBottomTabNavigator({
+  Home: HomeScreen,
+  About: AboutScreen,
+  Setting: SettingScreen
+})
+
+
+export default createAppContainer(tabNavigator)
+
+
+
+
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
